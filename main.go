436c09843404443a4bf0c1e0f32349c7e622dbe9
alveflo <victor.alveflo@gmail.com>
2020-03-27 19:40:24 +0100

    Add application start message

--- main.go
+++ main.go
@@ -4,11 +4,14 @@ import (
 	"github.com/gorilla/mux"
 	"log"
 	"net/http"
+	"fmt"
 )
 
 func main() {
 	r := mux.NewRouter()
 	r.HandleFunc("/", HomeHandler)
+
+	fmt.Println("\n - 🚀 Application launched @ http://:8080")
 	log.Fatal(http.ListenAndServe(":8080", r))
 }
 
